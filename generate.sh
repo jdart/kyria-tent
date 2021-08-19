4322982b7d9bb7c449217bc573e7db55e8ca6dc8
Jonathan Dart <jonathan.dart@boldcommerce.com>
2021-08-19 10:10:24 -0500

    more woopsie

--- generate.sh
+++ generate.sh
@@ -10,26 +10,26 @@ rm -rf ./tmp ./things
 mkdir tmp things
 cp ./*.dxf ./tmp/
 
-for ((i=1; i<=5; i++)); do
+for ((i=1; i<=7; i++)); do
     low=$((i*5+1))
-    high=$((low+5))
-    for ((j=low; j<high; j++)); do
+    high=$((low+4))
+    for j in $(seq -f "%02g" $low $high); do
         tmpFile="./tmp/tent.3x5.${j}deg.scad"
-        cp tent.3x5.scad $tmpFile
-        sed -i '' "s/tent_angle=.*/tent_angle=$j;/" $tmpFile
-        $openscad -o "./things/tent.3x5.${j}deg.stl" $tmpFile &
+        cp tent.3x5.scad "$tmpFile"
+        sed -i '' "s/tent_angle=.*/tent_angle=$j;/" "$tmpFile"
+        $openscad -o "./things/tent.3x5.${j}deg.stl" "$tmpFile" &
     done
     wait
 done
 
-for ((i=1; i<=5; i++)); do
+for ((i=1; i<=7; i++)); do
     low=$((i*5+1))
-    high=$((low+5))
-    for ((j=low; j<high; j++)); do
+    high=$((low+4))
+    for j in $(seq -f "%02g" $low $high); do
         tmpFile="./tmp/tent.3x6.${j}deg.scad"
-        cp tent.3x6.scad $tmpFile
-        sed -i '' "s/tent_angle=.*/tent_angle=$j;/" $tmpFile
-        $openscad -o "./things/tent.3x6.${j}deg.stl" $tmpFile &
+        cp tent.3x6.scad "$tmpFile"
+        sed -i '' "s/tent_angle=.*/tent_angle=$j;/" "$tmpFile"
+        $openscad -o "./things/tent.3x6.${j}deg.stl" "$tmpFile" &
     done
     wait
 done
